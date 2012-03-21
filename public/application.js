90cba598503daf042b6ececea426a8f825fa633e
Jason Draper <jason@drapergeek.com>
2012-03-21 09:46:08 -0400

    cleaned up the js a bit

--- public/application.js
+++ public/application.js
@@ -1,52 +1,52 @@
-      var ws;
+var ws;
+function adminChangeColor(color){
+  ws.send("change: " + color);
+}
 
-      function adminChangeColor(color){
-        ws.send("change: " + color);
-      }
+function change(color){
+  $("body").css("background-color", color);
+}
 
-      function change(color){
-        $("body").css("background-color", color);
-      }
+function socket() {
+  socket = new WebSocket("ws://ui.thoughtbot.com:9000");
+  socket.onmessage = function(event) {
+    console.log(event.data);
+    var command = event.data.split(": ");
+    var method = command[0];
+    var param = command[1];
+    window[method](param);
+  };
+  return socket;
+}
 
-      function socket() {
-        socket = new WebSocket("ws://ui.thoughtbot.com:9000");
-        socket.onmessage = function(event) {
-          console.log(event.data);
-          var command = event.data.split(": ");
-          var method = command[0];
-          var param = command[1];
-          window[method](param);
-        };
-        return socket;
-      }
+function ping(time){
+  console.log("ping: " + time);
+}
 
-      function ping(time){
-        console.log("ping: " + time);
-      }
+function ping_loop(socket){
+  setTimeout(function(){
+    socket.send("ping: " + new Date());
+    ping_loop(socket);
+  }, 1000);
+}
 
-      function ping_loop(socket){
-        setTimeout(function(){
-          socket.send("ping: " + new Date());
-          ping_loop(socket);
-        }, 1000);
-      }
 
-      $(function($) {
-        ws = socket();
-        //ping_loop(ws);
+$(function($) {
+  ws = socket();
+  //ping_loop(ws);
 
-        $('#send-change').submit(function() {
-          textBox = $("#send-change-text");
-          $.ajax({
-            url: "/push/" + textBox.val()
-           });
-           textBox.val("");
-          return false;
-        });
+  $('#send-change').submit(function() {
+    textBox = $("#send-change-text");
+    $.ajax({
+      url: "/push/" + textBox.val()
+    });
+    textBox.val("");
+    return false;
+  });
 
-        $("#show-admin").click(function(){
-          $("#send-change").show();
-          $("#show-admin").hide();
-        });
+  $("#show-admin").click(function(){
+    $("#send-change").show();
+    $("#show-admin").hide();
+  });
 
-      });
+});
