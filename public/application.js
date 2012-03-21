6c8a2453d7333b3d184cc8373a664fd4493d74fa
Jason Draper <jason@drapergeek.com>
2012-03-21 11:56:02 -0400

    set up animations for fades

--- public/application.js
+++ public/application.js
@@ -8,10 +8,29 @@ function send_push(command){
   });
 }
 
+
+
+function random_color(){
+   return '#'+Math.floor(Math.random()*16777215).toString(16);
+}
+
 function send_random_color(){
-   color = '#'+Math.floor(Math.random()*16777215).toString(16);
+   color = random_color();
    send_push(escape(color));
 }
+
+function send_random_fade(){
+  from_color = random_color();
+  to_color = random_color();
+  delay = 5000;
+
+  data = "from_color=" + escape(from_color) + "&to_color=" + escape(to_color) + "&delay=" + escape(delay)
+  $.ajax({
+    url: "/fade",
+    data: data
+  });
+}
+
 
 function setup_pusher(){
   var pusher = new Pusher('d141f03421d0c08db875');
@@ -19,24 +38,27 @@ function setup_pusher(){
   channel.bind('change', function(data) {
     change_page_color(data.message);
   });
+
+  channel.bind('events', function(data){
+    if (data.event == "fade") {
+      fadeInBox(data.options.from_color, data.options.to_color, parseInt(data.options.delay));
+    };
+  });
+}
+
+function fadeInBox(from_color, to_color, delay){
+  $("#box2").hide();
+  $("#box1").css("background-color", from_color);
+  $("#box2").css("background-color", to_color);
+  $("#box1").show();
+  $("#box2").fadeIn(delay);
+  $("#box1").fadeOut(delay);
+
 }
 
 
 $(function($) {
   setup_pusher();
-
-  $('#send-change').submit(function() {
-    textBox = $("#send-change-text");
-    if (textBox.val()=="") {
-      alert("You must enter a command!");
-    }
-    else{
-      send_push(textBox.val());
-    };
-    textBox.val("");
-    return false;
-  });
-
 
   $("#show-admin").click(function(){
     $("#send-change").show();
@@ -47,5 +69,10 @@ $(function($) {
     send_random_color();
     return false;
   });
+
+  $("#random-fade").click(function(){
+    send_random_fade();
+  });
+
 
 });
--- server.rb
+++ server.rb
@@ -2,6 +2,13 @@ require 'rubygems'
 require 'sinatra'
 require 'pusher'
 
+
+def setup_pusher
+  Pusher.app_id = '17217'
+  Pusher.key = 'd141f03421d0c08db875'
+  Pusher.secret = '564af0b867bdb4739df8'
+end
+
 get '/' do
   erb :admin
 end
@@ -9,12 +16,23 @@ end
 
 get '/push/:command' do
   puts params[:command]
-  Pusher.app_id = '17217'
-  Pusher.key = 'd141f03421d0c08db875'
-  Pusher.secret = '564af0b867bdb4739df8'
-
+  setup_pusher
   data = {'message' => params[:command]}
   Pusher['colors'].trigger('change', data)
+end
+
+get '/fade' do
+  setup_pusher
+  puts params.inspect
+  data = {'event' => 'fade', 
+          'time_delay' => 0, 
+          'options' => 
+              {'from_color' => params[:from_color], 
+                'to_color' =>  params[:to_color],
+                'delay' => params[:delay]
+              }
+         }
+  Pusher['colors'].trigger('events', data)
 end
 
 
