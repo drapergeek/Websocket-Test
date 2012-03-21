a0c99459adf239dc8140e720235cc296d8e3febd
Jason Draper <jason@drapergeek.com>
2012-03-21 09:41:08 -0400

    changed the form to go to a server action to be able to use pusher

--- server.rb
+++ server.rb
@@ -2,13 +2,12 @@ require 'rubygems'
 require 'sinatra'
 
 get '/' do
-  erb :admin, :format=>:html5
-end
-
-get '/admin' do
   erb :admin
 end
 
-get '/cordova-1.5.0.js' do
-  erb :cordova
+
+get '/push/:command' do
+  puts params[:command]
 end
+
+
